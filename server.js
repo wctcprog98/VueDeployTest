1fc91e63c414a5ee8f6e173e2824360b36689ed1
Nenye <chinenyediei@gmail.com>
2021-09-17 02:32:26 -0500

    changed port to PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 //serves files from our dist directory where index.html file is located
 app.use('/', serveStatic(path.join(__dirname, '/dist/')))
 
-const port = process.env.port || 8080; 
+const port = process.env.PORT || 8080; 
 app.listen(port);
 
 console.log("Listening on port: " + port); 
